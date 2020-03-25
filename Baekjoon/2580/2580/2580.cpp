eb7fc91ee949a30adba934e423acfe8d14373d0d
Jaehoon <pf7@naver.com>
2020-03-25 16:43:40 +0900

    backtrack(9663, 2580)

---
+++ Baekjoon/2580/2580/2580.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define BLOCK_IDX(i,j) ((i-1)/3*3 + (j+2)/3)
+
+vector<vector<int>> table(10, vector<int>(10, 0));
+vector<pair<int, int>> zeroes;
+
+vector<vector<bool>> row_candidate(10, vector<bool>(10,false));
+vector<vector<bool>> col_candidate(10, vector<bool>(10, false));
+vector<vector<bool>> block_candidate(10, vector<bool>(10, false));
+
+bool fill_sudoku(int zidx) {
+
+    if (zidx >= zeroes.size()) return true;
+    
+    int tx = zeroes[zidx].first;
+    int ty = zeroes[zidx].second;
+
+    int ret = false;
+
+    for (int i = 1; i < 10;i++) {
+        if (!row_candidate[tx][i] &&
+            !col_candidate[ty][i] &&
+            !block_candidate[BLOCK_IDX(tx, ty)][i])
+        {
+            table[tx][ty] = i;
+
+            row_candidate[tx][i] = true;
+            col_candidate[ty][i] = true;
+            block_candidate[BLOCK_IDX(tx, ty)][i] = true;
+
+            if (fill_sudoku(zidx + 1)) {
+                ret = true;
+                break;
+            }
+
+            row_candidate[tx][i] = false;
+            col_candidate[ty][i] = false;
+            block_candidate[BLOCK_IDX(tx, ty)][i] = false;
+
+            table[tx][ty] = 0;
+        }
+    }
+
+    return ret;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int N = 9;
+
+    int val;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> val;
+            table[i][j] = val;
+            row_candidate[i][val] = true;
+            col_candidate[j][val] = true;
+            block_candidate[ BLOCK_IDX(i,j)][val] = true;
+            if (val == 0) {
+                zeroes.push_back(make_pair(i,j));
+            }
+        }
+    }
+
+    fill_sudoku(0);
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            printf("%d ", table[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
--- Baekjoon/9663/9663/9663.cpp
+++ Baekjoon/9663/9663/9663.cpp
@@ -1,9 +1,51 @@
 ﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<bool> sum;
+vector<bool> sub;
+vector<bool> row_fill;
+
+//increasing column
+int nqueens(int col, int N) {
+	if (col > N) {
+		return 1;
+	}
+	
+	int ret = 0;
+
+	for (int row = 1; row <= N; row++) {
+		if (!row_fill[row] &&
+			!sum[row + col - 2] &&
+			!sub[row - col + (N - 1)]) {
+
+			row_fill[row] = true;
+			sum[row + col - 2] = true;
+			sub[row - col + (N - 1)] = true;
+
+			ret += nqueens(col + 1, N);
+
+			row_fill[row] = false;
+			sum[row + col - 2] = false;
+			sub[row - col + (N - 1)] = false;
+		}
+	}
+
+	return ret;
+}
+
 
 int main()
 {
+	int N;
+	cin >> N;
 
-
+	sum = vector<bool>(2 * N - 1, false);
+	sub = vector<bool>(2 * N - 1, false);
+	row_fill = vector<bool>(N + 1, false);
+	
+	cout << nqueens(1, N) << endl;
 
 	return 0;
 }
