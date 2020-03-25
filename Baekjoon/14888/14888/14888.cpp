4e1af335bed1b11913f5c65b7b7dd716a84da8e4
Jaehoon <pf7@naver.com>
2020-03-25 23:17:36 +0900

    backtrack(14888, 14889)

---
+++ Baekjoon/14888/14888/14888.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef struct minmax_t {
+    int minval;
+    int maxval;
+} minmax;
+
+vector<int> arr;
+vector<int> op;
+
+minmax calculate(int idx, int val) {
+
+    if (idx == arr.size()) return { val, val };
+
+    vector<minmax> out;
+
+    if (op[0] > 0) {
+        op[0]--;
+        out.push_back(calculate(idx + 1, val + arr[idx]));
+        op[0]++;
+    }
+    if (op[1] > 0) {
+        op[1]--;
+        out.push_back(calculate(idx + 1, val - arr[idx]));
+        op[1]++;
+    }
+    if (op[2] > 0) {
+        op[2]--;
+        out.push_back(calculate(idx + 1, val * arr[idx]));
+        op[2]++;
+    }
+    if (op[3] > 0) {
+        op[3]--;
+        out.push_back(calculate(idx + 1, val / arr[idx]));
+        op[3]++;
+    }
+
+    minmax minmaxout;
+    minmaxout.minval = out[0].minval;
+    minmaxout.maxval = out[0].maxval;
+
+    for (int i = 1; i < out.size(); i++) {
+        if (out[i].minval < minmaxout.minval) {
+            minmaxout.minval = out[i].minval;
+        }
+        if (out[i].maxval > minmaxout.maxval) {
+            minmaxout.maxval = out[i].maxval;
+        }
+    }
+
+    return minmaxout;
+}
+
+int main()
+{
+    int N;
+    
+    cin >> N;
+
+    arr = vector<int>(N, 0);
+    op = vector<int>(4, 0);
+
+    int val;
+    int idx = 0;
+    while (idx < N) {
+        cin >> arr[idx];
+        idx++;
+    }
+
+    idx = 0;
+    while (idx < 4) {
+        cin >> op[idx];
+        idx++;
+    }
+
+    minmax out = calculate(1, arr[0]);
+
+    printf("%d\n%d", out.maxval, out.minval);
+
+    return 0;
+}
---
+++ Baekjoon/14889/14889/14889.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#define MAX 987654321
+
+using namespace std;
+
+int N;
+vector<vector<int>> sngy;
+vector<int> teamA;
+vector<int> teamB;
+
+int pick(int valA, int valB, int idx) {
+
+    if (idx > N) return abs(valA - valB);
+
+    int ret = MAX;
+    int A = valA;
+    int B = valB;
+
+    if (teamA.size() < N / 2) {
+        for (int i = 0; i < teamA.size(); i++) {
+            A += sngy[teamA[i]][idx];
+        }
+        teamA.push_back(idx);
+        int result = pick(A, valB, idx + 1);
+        teamA.pop_back();
+        if (ret > result) {
+            ret = result;
+        }
+    }
+
+    if (teamB.size() < N / 2) {
+        for (int i = 0; i < teamB.size(); i++) {
+            B += sngy[teamB[i]][idx];
+        }
+        teamB.push_back(idx);
+        int result = pick(valA, B, idx + 1);
+        teamB.pop_back();
+        if (ret > result) {
+            ret = result;
+        }
+    }
+
+    return ret;
+}
+
+int main()
+{
+    cin >> N;
+
+    sngy = vector<vector<int>>(N+1, vector<int>(N+1, 0));
+
+
+    teamA.reserve(N / 2);
+    teamB.reserve(N / 2);
+
+    int val;
+    int idx = 1;
+    while (idx <= N) {
+        int idx2 = 1;
+        while (idx2 <= N) {
+            cin >> val;
+            if (idx < idx2) {
+                sngy[idx][idx2] += val;
+            }
+            else {
+                sngy[idx2][idx] += val;
+            }
+            idx2++;
+        }
+        idx++;
+    }
+        
+    printf("%d\n", pick(0, 0, 1));
+
+    return 0;
+}
