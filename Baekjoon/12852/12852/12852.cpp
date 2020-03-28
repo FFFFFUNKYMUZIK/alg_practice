44ec4a0e55a26d28cf5d96e91219afbf2272d85f
Jaehoon <pf7@naver.com>
2020-03-29 02:38:22 +0900

    DP&Backtrack(12852, 14002)

---
+++ Baekjoon/12852/12852/12852.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#define INTMAX 0x7FFFFFFF
+
+using namespace std;
+
+vector<int> dp;
+
+void backtrack(int N) {
+    printf("%d ", N);
+    if (N == 1) return;
+    
+    int& ret = dp[N];
+    if ((N % 3 == 0) && (dp[N / 3] == (ret - 1))) backtrack(N / 3);
+    else if ((N % 2 == 0) && (dp[N / 2] == (ret - 1))) backtrack(N / 2);
+    else backtrack(N - 1);
+}
+
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    dp = vector<int>(N+1, INTMAX);
+    dp[1] = 0;
+
+    for (int i = 2; i <= N; i++) {
+        int& ret = dp[i];
+        if (i % 3 == 0) ret = min(ret, dp[i / 3]+1);
+        if (i % 2 == 0) ret = min(ret, dp[i / 2]+1);
+        ret = min(ret, dp[i - 1]+1);
+    }
+
+    printf("%d\n", dp[N]);
+
+    backtrack(N);
+
+    return 0;
+}
---
+++ Baekjoon/14002/14002/14002.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> arr(N+1, 0);
+    vector<int> len(N+1, 0);
+
+    int idx = 1;
+    while (idx <= N) {
+        cin >> arr[idx];
+        idx++;
+    }
+
+    int maxidx = 1;
+    int maxlen = 1;
+
+    for (int i = 1; i <= N; i++) {
+        int maxval = 0;
+        for (int j = 1; j < i; j++) {
+            if (maxval < len[j] && arr[j] < arr[i]) maxval = len[j];
+        }
+        len[i] = maxval + 1;
+        if (len[i] > maxlen) {
+            maxlen = len[i];
+            maxidx = i;
+        }
+    }
+
+    printf("%d\n", maxlen);
+
+    int i = maxidx;
+    vector<int> st;
+    while (i>0){
+        st.push_back(arr[i]);
+        if (i == 1) break;
+
+        int j = i-1;
+        while (j > 0) {
+            if ((len[j] == len[i] - 1) && (arr[j] < arr[i])) {
+                break;
+            }
+            j--;
+        }
+        i = j;
+    }
+
+    for (auto it = st.rbegin(); it != st.rend(); it++) {
+        printf("%d ", *it);
+    }
+
+    return 0;
+}
