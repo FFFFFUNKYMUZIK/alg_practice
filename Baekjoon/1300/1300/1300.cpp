65cbda95d0f743840ae62d1130597515784c2ff8
Jaehoon <pf7@naver.com>
2020-04-02 10:34:22 +0900

    binsearch(1300)

---
+++ Baekjoon/1300/1300/1300.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N, k;
+    cin >> N >> k;
+
+    int s = 1, e = k;
+
+    int ret = 1;
+    //O(N^2log(k))
+    while (s <= e) {
+        int cnt = 0;
+        int mid = (s + e) / 2;
+
+        for (int i = 1; i <= N; i++) {
+            cnt += min(mid / i, N);
+        }
+
+        if (cnt < k) {
+            s = mid + 1;
+        }
+        else {
+            ret = mid;
+            e = mid - 1;
+        }
+    }
+
+    printf("%d\n", ret);
+
+    return 0;
+}
