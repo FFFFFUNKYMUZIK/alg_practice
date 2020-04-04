92747d39e0933b44e2c96ea54e39877e212be583
Jaehoon <pf7@naver.com>
2020-04-04 22:34:30 +0900

    Leetcode 617, 828

---
+++ Leetcode/617/617/617.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    
+};
+#if 0
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (t1 == nullptr && t2 == nullptr) return nullptr;
+
+        int sum = 0;
+
+        if (t1 != nullptr) sum += t1->val;
+        if (t2 != nullptr) sum += t2->val;
+        
+        TreeNode* newnode = new TreeNode(sum);
+        
+        if (t1 != nullptr) {
+            newnode->left = mergeTrees(t1->left, newnode->left);
+            newnode->right = mergeTrees(t1->right, newnode->right);
+            delete t1;
+        }
+        if (t2 != nullptr) {
+            newnode->left = mergeTrees(t2->left, newnode->left);
+            newnode->right = mergeTrees(t2->right, newnode->right);
+            delete t2;
+        }
+
+        return newnode;
+    }
+
+};
+
+#elif 0
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (t1 == nullptr && t2 == nullptr) return nullptr;
+
+        int sum = 0;
+      
+        TreeNode* l1 = nullptr, * r1 = nullptr, * l2 = nullptr, * r2 = nullptr;
+        TreeNode* newnode;
+
+        if (t1 != nullptr) {
+            l1 = t1->left;
+            r1 = t1->right;
+            if (t2 != nullptr) {
+                t1->val += t2->val;
+                l2 = t2->left;
+                r2 = t2->right;
+                delete t2;
+            }
+            newnode = t1;
+        }
+        else {
+            l2 = t2->left;
+            r2 = t2->right;
+            newnode = t2;
+        }
+
+        newnode->left = mergeTrees(l1, l2);
+        newnode->right = mergeTrees(r1, r2);
+
+        return newnode;
+    }
+
+};
+
+#else
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+
+        if (t1 == nullptr) return t2;
+        if (t2 == nullptr) return t1;
+
+        t1->val += t2->val;
+
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+
+        //delete t2;
+
+        return t1;
+    }
+
+};
+
+
+
+
+#endif
+
+
+int main()
+{
+    Solution sol;
+    
+    return 0;
+}
---
+++ Leetcode/828/828/828.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define MOD (1000000000+7)
+#define NUM_ALPHABET 26
+
+class Solution {
+
+public:
+
+    vector<int> pidx = vector<int>(NUM_ALPHABET, -1);
+    vector<int> ppidx = vector<int>(NUM_ALPHABET, -1);
+
+
+    int uniqueLetterString(string s) {
+        if (s.length() == 0) return 0;
+        if (s.length() == 1) return 1;
+
+        vector<int> dp(s.length(), 0);
+
+        dp[0] = 1;
+        dp[1] = s[0] == s[1]? 2 : 4;
+
+        pidx[s[0] - 'A'] = 0;
+        if (s[0] == s[1]) {
+            pidx[s[0] - 'A'] = 1;
+            ppidx[s[0] - 'A'] = 0;
+        }
+        else {
+            pidx[s[1] - 'A'] = 1;
+        }
+
+        for (int i = 2; i < s.length(); i++) {
+            int cidx = s[i] - 'A';
+            //dp[i] = (dp[i - 1] + (dp[i - 1] - dp[i - 2]) + (i-pidx[cidx]) - (pidx[cidx] - ppidx[cidx]))%MOD;
+            dp[i] = (2*dp[i - 1] - dp[i - 2] + i - 2* pidx[cidx] + ppidx[cidx]) % MOD;
+            ppidx[cidx] = pidx[cidx];
+            pidx[cidx] = i;
+        }
+
+        return dp[s.length() - 1];
+    }
+};
+
+int main()
+{
+    Solution sol;
+    
+    string test("ABA");
+
+    printf("%d\n", sol.uniqueLetterString(test));
+
+    return 0;
+}
