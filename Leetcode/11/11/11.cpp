f80cd20bf9aa2e4185aab38b535e02b28c063c17
Jaehoon <pf7@naver.com>
2020-04-07 20:02:37 +0900

    Leetcode 11,12

---
+++ Leetcode/11/11/11.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#if 0
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0, j = height.size() - 1;
+        
+        int lh = -1, rh = -1;
+        int max_area = 0;
+        int cur_area = 0;
+
+        while (i < j) {
+            if (lh < height[i]) {
+                if (rh < height[j]) {
+                    int part_height;
+
+                    if (height[j] <= height[i]) {
+                        rh = height[j];
+                        part_height = height[j];
+                    }
+                    else {
+                        lh = height[i];
+                        part_height = height[i];
+                    }
+
+                    cur_area = (j - i) * part_height;
+                    if (cur_area > max_area) max_area = cur_area;
+
+                }
+                else {
+                    j--; continue;
+                }
+            }
+            else {
+                i++; continue;
+            }
+        }
+
+        return max_area;
+
+    }
+};
+
+#else
+
+#include <algorithm>
+
+//reference sol
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int res = 0;
+        int i = 0, j = height.size() - 1;
+
+        while (i < j) {
+            res = max(res, (j - i) * min(height[i], height[j]));
+            if (height[i] <= height[j]) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+
+        return res;
+    }
+};
+
+#endif
+
+int main()
+{
+    Solution sol;
+
+    vector<int> input;
+    
+    int ret;
+
+    input = vector<int>({ 1,8,6,2,5,4,8,3,7 });
+    ret = sol.maxArea(input);
+    input = vector<int>({ 1, 2 });
+    ret = sol.maxArea(input);
+
+    return 0;
+}
---
+++ Leetcode/12/12/12.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {
+
+        string ret;
+        vector<int> val({1,4, 5, 9, 10,40, 50, 90, 100, 400, 500,900, 1000});
+        vector<string> ch({ string("I"), string("IV"), string("V"), string("IX"), string("X"), string("XL"), string("L"), string("XC"), string("C"), string("CD"), string("D"), string("CM"), string("M") });
+        unordered_map<int, string> cm;
+
+        for (int i = 0; i < val.size(); i++) {
+            cm[val[i]] = ch[i];
+        }
+
+        int q;
+        for (int i = val.size()-1; i >=0; i--) {
+            q = num / val[i];
+            for (int j = 0; j < q; j++) {
+                ret+=cm[val[i]];
+            }
+            if ((num -= q * val[i]) == 0) break;
+        }
+
+        return ret;
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    string ret;
+    ret = sol.intToRoman(3);
+    ret = sol.intToRoman(4);
+    ret = sol.intToRoman(9);
+    ret = sol.intToRoman(58);
+    ret = sol.intToRoman(1994);
+
+    return 0;
+}
