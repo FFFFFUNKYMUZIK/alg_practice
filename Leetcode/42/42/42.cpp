4377560f8278eebfe70ca44e59334b569e754cab
Jaehoon <pf7@naver.com>
2020-04-21 23:58:55 +0900

    Leetcode 42,43

---
+++ Leetcode/42/42/42.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+        stack<int> st;
+
+        int water = 0;
+        for (int i = 0; i < height.size(); i++) {
+            int pre = 0;
+            while (!st.empty() && height[st.top()] <= height[i]) {
+                water += (i - st.top() - 1) * (height[st.top()] - pre);
+                pre = height[st.top()];
+                st.pop();
+            }
+            if (!st.empty()) {
+                water += (i - st.top() - 1) * (height[i] - pre);
+            }
+            st.push(i);
+        }
+
+        return water;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    int ret;
+    vector<int> in;
+
+    in = vector<int>({ 0,1,0,2,1,0,1,3,2,1,2,1 });
+    ret = sol.trap(in);
+
+    in = vector<int>({ 0, 8, 9, 2, 4, 6, 1, 3, 5, 7, 45, 73, 7623, 24, });
+    ret = sol.trap(in); 
+
+
+    return 0;
+}
---
+++ Leetcode/43/43/43.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+
+        int n1 = num1.length();
+        int n2 = num2.length();
+
+        if (n1 == 0 || n2 == 0) return string("");
+
+        vector<int> round(n1 + n2, 0);
+
+        string ret;
+        int c1, c2, mul;
+        for (int i = 0; i < num1.length(); i++) {
+            for (int j = 0; j < num2.length() ; j++) {
+                c1 = num1[i] - '0';
+                c2 = num2[j] - '0';
+                if (c1 == 0 || c2 == 0) continue;
+                mul = c1 * c2;
+                round[i + j] += mul / 10;
+                round[i + j + 1] += mul % 10;
+            }
+        }
+
+        
+        for (int i = round.size() - 1; i >= 0; i--) {
+            if (i > 0) round[i-1] += round[i] / 10;
+            ret = to_string((round[i]%10)) + ret;
+        }
+
+        int i = 0;
+        while (i < ret.size()-1 && ret[i] == '0') {
+            i++;
+        }
+
+        return ret.substr(i);
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    string ret;
+    string in1, in2;
+    
+    in1 = "2"; in2 = "3";
+    ret = sol.multiply(in1, in2);
+
+    in1 = "123"; in2 = "456";
+    ret = sol.multiply(in1, in2);
+
+    in1 = "0"; in2 = "0";
+    ret = sol.multiply(in1, in2);
+
+    return 0;
+}
