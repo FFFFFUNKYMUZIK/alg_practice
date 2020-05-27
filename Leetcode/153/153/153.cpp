c5dd25783537e7e61e335745f2d5df2f639ca85b
Jaehoon <pf7@naver.com>
2020-05-27 23:44:17 +0900

    Leetcode 153~155

---
+++ Leetcode/153/153/153.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+problem 153 doesn't allow duplicate element, otherwise problem 154 does.
+Solution for 154 can beat prob.153.
+following is for prob. 154.
+*/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int s = 0, e = nums.size() - 1;
+
+        while (s < e) {
+            int mid = (s + e) / 2;
+
+            if (nums[s] < nums[mid]) {
+                if (nums[s] < nums[e]) {
+                    return nums[s];
+                }
+                else {
+                    s = mid + 1;
+                }
+            }
+            else if (nums[s] == nums[mid]) {
+                if (nums[s] < nums[e]) {
+                    return nums[s];
+                }
+                else if(nums[s]==nums[e]){
+                    //worst case O(n)
+                    s++;
+                }
+                else {
+                    s = mid + 1;
+                }
+            }
+            else {
+                s++;
+                e = mid;
+            }
+
+        }
+
+        //should not reach if input array is valid
+        return nums[s];
+
+    }
+};
+
+int main()
+{
+    Solution sol;
+    int ret;
+
+    vector<int> in = vector<int>({3,4,5,1,2});
+    ret = sol.findMin(in);
+
+    in = vector<int>({ 4,5,6,7,0,1,2 });
+    ret = sol.findMin(in);
+
+    in = vector<int>({ 2,2,2,0,1 });
+    ret = sol.findMin(in);
+
+    return 0;
+}
---
+++ Leetcode/155/155/155.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+Constraints:
+
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+*/
+
+class MinStack {
+public:
+
+    vector<pair<int, int>> bucket;
+
+    /** initialize your data structure here. */
+    MinStack() {
+        bucket.reserve(10);
+    }
+
+    void push(int x) {
+        if (bucket.size() == 0) {
+            bucket.push_back(make_pair(x, x));
+        }
+        else {
+            int minval = bucket.back().second;
+            if (minval < x) {
+                bucket.push_back(make_pair(x, minval));
+            }
+            else {
+                bucket.push_back(make_pair(x, x));
+            }
+        }
+    }
+
+    void pop() {
+        bucket.pop_back();
+    }
+
+    int top() {
+        return bucket.back().first;
+    }
+
+    int getMin() {
+        return bucket.back().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+int main()
+{
+    int ret;
+    MinStack minStack = MinStack();
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    ret = minStack.getMin(); // return -3
+    minStack.pop();
+    ret = minStack.top();    // return 0
+    ret = minStack.getMin(); // return -2
+
+    return 0;
+}
