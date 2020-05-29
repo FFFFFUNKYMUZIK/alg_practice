413d19f64fb314b272416042d809c99cb59ecfb5
Jaehoon <pf7@naver.com>
2020-05-29 21:04:25 +0900

    Leetcode 174, 175

---
+++ Leetcode/175/175/175.cpp
@@ -0,0 +1,10 @@
+﻿/*
+
+# Write your MySQL query statement below
+
+//453 ms, 42.41%
+select FirstName, LastName, City, State from Person left join Address on Person.PersonId = Address.PersonId
+
+performance(runtime) evaluation is not reliable... 
+
+*/
