d75ab5df4c128d85d55c7815a0e32454895a0c0c
Jaehoon <pf7@naver.com>
2020-06-14 21:12:26 +0900

    Leetcode 205, 206

---
+++ Leetcode/205/205/205.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+Note:
+You may assume both s and t have the same length.
+*/
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int len = s.length();
+        if (len == 0) return true;
+
+        vector<int> cmap(256, -1);
+        vector<int> comap(256, -1);
+
+        for (int i = 0; i < len; i++) {
+            if (cmap[s[i]] == -1) {
+                if (comap[t[i]] != -1) return false;
+                cmap[s[i]] = t[i];
+                comap[t[i]] = 1;
+            }
+            else if (cmap[s[i]] != t[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    bool ret;
+
+    ret = sol.isIsomorphic(string("egg"), string("add"));
+
+    return 0;
+}
---
+++ Leetcode/206/206/206.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/*
+Follow up:
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
+
+#if 0
+
+//iterative solution
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head) return head;
+
+        ListNode* front, * back;
+        back = head->next;
+        front = head;
+        front->next = nullptr;
+
+        ListNode* nextback;
+        while (back) {
+            nextback = back->next;
+            back->next = front;
+            front = back;
+            back = nextback;
+        }
+        return front;
+    }
+};
+
+#else
+
+//recursive solution
+class Solution {
+public:
+
+    using pLL = pair < ListNode*, ListNode * >;
+
+    pLL reversePart(ListNode* head) {
+        if (!head->next) return make_pair(head, head);
+
+        pLL ret = reversePart(head->next);
+
+        ret.second->next = head;
+        ret.second = head;
+        return ret;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        if (!head) return head;
+        
+        pLL ret = reversePart(head);
+        ret.second->next = nullptr;
+
+        return ret.first;
+    }
+};
+
+
+#endif
+
+
+int main()
+{
+    return 0;
+}
